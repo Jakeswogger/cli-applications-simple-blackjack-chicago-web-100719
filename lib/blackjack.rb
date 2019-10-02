3a5695313f30a72929f650f48490f5b878733ef2
Jake Swogger <jakeswogger1@gmail.com>
2019-10-02 01:51:33 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,51 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  card = rand(1..11)
+  card
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  num = deal_card + deal_card
+  
+  display_card_total(num)
+  num
 end
 
-def hit?
-  # code hit? here
+def hit?(num)
+  prompt_user
+  h_s = get_user_input
+  if h_s == "s"
+       num
+    elsif h_s == "h"
+       num += deal_card
+    else 
+        invalid_command
+        prompt_user 
+        get_user_input
+      end 
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -39,6 +53,17 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  user_cards = initial_round
+  prompt_user
+  until user_cards > 21
+  if get_user_input == "s"
+    prompt_user
+  end
+  if get_user_input == "h"
+    user_cards += deal_card
+  end
+  p user_cards
+end
 end
     
